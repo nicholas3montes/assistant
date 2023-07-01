939645c51d20fb78e1544557834e5e48d1ceeed2
Nicholas <nicholas@laager.com>
2023-07-01 19:19:59 -0300

    Reading folder files

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :input, only: [:index]
+  root "inputs#index"
+
+  get "/inputs", to: "inputs#index"
 end
